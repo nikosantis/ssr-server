a3a80bab067079c7bc3635c71a73f2a5a1e327fd
Niko Santis <nikosantis@gmail.com>
2019-10-28 13:29:51 -0300

    28 oct snyk

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const helmet = require('helmet');
 const passport = require('passport');
+const session = require('express-session');
 const boom = require('@hapi/boom');
 const cookieParser = require('cookie-parser');
 const axios = require('axios');
@@ -7,13 +9,29 @@ const axios = require('axios');
 const { config } = require('./config');
 
 const app = express();
+app.use(helmet());
 
 // body parser
 app.use(express.json());
 app.use(cookieParser());
+app.use(session({ secret: config.sessionSecret }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 //  Basic strategy
 require('./utils/auth/strategies/basic');
+
+// OAuth strategy
+require('./utils/auth/strategies/oauth');
+
+// Google strategy
+require('./utils/auth/strategies/google');
+
+// Twitter strategy
+require('./utils/auth/strategies/twitter');
+
+// Facebook strategy
+require('./utils/auth/strategies/facebook');
 
 app.post('/auth/sign-in', async function(req, res, next) {
   passport.authenticate('basic', function(error, data) {
@@ -102,6 +120,100 @@ app.delete('/user-movies/:userMovieId', async function(req, res, next) {
     next(error);
   }
 });
+
+app.get(
+  '/auth/google-oauth',
+  passport.authenticate('google-oauth', {
+    scope: ['email', 'profile', 'openid']
+  })
+);
+
+app.get(
+  '/auth/google-oauth/callback',
+  passport.authenticate('google-oauth', { session: false }),
+  function(req, res, next) {
+    if (!req.user) {
+      next(boom.unauthorized());
+    }
+
+    const { token, ...user } = req.user;
+
+    res.cookie('token', token, {
+      httpOnly: !config.dev,
+      secure: !config.dev
+    });
+
+    res.status(200).json(user);
+  }
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['email', 'profile', 'openid']
+  })
+);
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', { session: false }),
+  function(req, res, next) {
+    if (!req.user) {
+      next(boom.unauthorized());
+    }
+
+    const { token, ...user } = req.user;
+
+    res.cookie('token', token, {
+      httpOnly: !config.dev,
+      secure: !config.dev
+    });
+
+    res.status(200).json(user);
+  }
+);
+
+app.get('/auth/twitter', passport.authenticate('twitter'));
+
+app.get(
+  '/auth/twitter/callback',
+  passport.authenticate('twitter', { session: false }),
+  function(req, res, next) {
+    if (!req.user) {
+      next(boom.unauthorized());
+    }
+
+    const { token, ...user } = req.user;
+
+    res.cookie('token', token, {
+      httpOnly: !config.dev,
+      secure: !config.dev
+    });
+
+    res.status(200).json(user);
+  }
+);
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get(
+  '/auth/facebook/callback',
+  passport.authenticate('facebook', { session: false }),
+  function(req, res, next) {
+    if (!req.user) {
+      next(boom.unauthorized());
+    }
+
+    const { token, ...user } = req.user;
+
+    res.cookie('token', token, {
+      httpOnly: !config.dev,
+      secure: !config.dev
+    });
+
+    res.status(200).json(user);
+  }
+);
 
 app.listen(config.port, function() {
   console.log(`Listening http://localhost:${config.port}`);
